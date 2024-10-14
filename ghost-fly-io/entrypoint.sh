27b26cb4e15fc469f68291a760e3b20c151c7848
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-10-15 01:03:07 +0200

    do not run docker-entrypoint.sh as it tries to chown /var/lib/ghost/content which GeeseFS hangs itself up on

--- ghost-fly-io/entrypoint.sh
+++ ghost-fly-io/entrypoint.sh
@@ -40,14 +40,28 @@ mount_s3() {
   fi
 }
 
+init_ghost_content() {
+  # Copied from Ghost's docker-entrypoint.sh
+  baseDir="$GHOST_INSTALL/content.orig"
+  for src in "$baseDir"/*/ "$baseDir"/themes/*; do
+    src="${src%/}"
+    target="$GHOST_CONTENT/${src#$baseDir/}"
+    mkdir -p "$(dirname "$target")"
+    if [ ! -e "$target" ]; then
+      tar -cC "$(dirname "$src")" "$(basename "$src")" | tar -xC "$(dirname "$target")"
+    fi
+  done
+}
+
 main() {
   mount_s3
   export NODE_ENV=development # To be able to use SQlite
   export LITESTREAM_DATABASE_PATH="/db.sqlite"
   export database__connection__filename="$LITESTREAM_DATABASE_PATH"
   export BUCKET_PATH="ghost.db"
+  init_ghost_content
   maybe_idle
-  info_run exec /usr/local/bin/docker-entrypoint.sh node current/index.js
+  info_run exec node current/index.js
 }
 
 main "$@"
